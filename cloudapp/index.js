f48c5439805819f0c7e936d3d37adce99d9a14e8
sdp-srh <105343612+sdp-srh@users.noreply.github.com>
2025-04-02 12:01:38 +0200

    extended to cloud app

--- cloudapp/index.js
+++ cloudapp/index.js
@@ -4,12 +4,8 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 
 
-app.get('/', function (req, res) {
-  res.send(`
-    <h1>Hello ACS Course</h1>
-    <p>My first Express app!</p>
-    <p>and running on Google Cloud Platform</p>
-  `)
+app.get('/api', function (req, res) {
+  res.send('here will be the api')
 })
 
 app.listen(port, () => {
