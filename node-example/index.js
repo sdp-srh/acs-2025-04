aab3377c283c996d84a6176d0503ed818b86cb81
sdp-srh <105343612+sdp-srh@users.noreply.github.com>
2025-04-02 15:04:33 +0200

    index.js text updated

--- node-example/index.js
+++ node-example/index.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) {
   res.send(`
     <h1>Hello ACS Course</h1>
     <p>My first Express app!</p>
-    <p>and running on Google Cloud Platform</p>
+    <p>and running on Google Cloud Platform (02.04.2025)</p>
   `)
 })
 
